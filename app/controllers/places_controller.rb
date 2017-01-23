a310c03d3f4048a6a8ae09811db2284260e84136
Jade Vargas <jadevargas92@gmail.com>
2017-01-23 04:26:30 +0000

    Added edit and delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,28 @@ class PlacesController < ApplicationController
 		@place = Place.new
 	end
 
+	def show
+		@place = Place.find(params[:id])
+	end
+
 	def create
 		current_user.places.create(place_params)
+		redirect_to root_path
+	end
+
+	def edit
+		@place = Place.find(params[:id])
+	end
+
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
 		redirect_to root_path
 	end
 
